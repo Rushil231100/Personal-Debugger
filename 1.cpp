90ab2625a4d8257fb9920ad889dc32b8bf1366a4
RUSHIL KAUSHAL SANGHAVI <50961163+Rushil231100@users.noreply.github.com>
2020-01-09 20:30:21 +0530

    First code for "=" using stl
    1 file is source 
    and target is what delivered
    Problem: the target runs perfectly when compiled in different IDE ,but shows Winmain missing error in same console application

---
+++ 1.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+ int main()
+ {
+ 	int i;
+ 	int j;
+ 	
+ 	i=7;
+ 	//cout<<i;
+ 	i+=5;
+ 	j     =999;
+ 	return 0;
+ }
---
+++ target1.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+using namespace std;
+ int main()
+ {
+ 	int i;
+ 	int j;
+ 	
+ 	i=7;
+cout<<"	i  = "<<	i <<endl;
+ 	//cout<<i;
+ 	i+=5;
+cout<<"	i  = "<<	i <<endl;
+ 	j     =999;
+cout<<"	j  = "<<	j <<endl;
+ 	return 0;
+ }
+
